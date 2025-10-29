bdc5d5b5c2df1ff5946d54e94253e4f2f86b9d48
Maximiliano Moreira <maximiliano.moreira@estudiantes.utec.edu.uy>
2025-10-29 20:13:54 -0300

    Creacion

---
+++ src/main/java/com/example/demo/db/databaseConection.java
@@ -0,0 +1,50 @@
+package com.example.demo.db;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class databaseConection {
+
+    private static Properties reader = new Properties();
+    private static databaseConection instancia;
+    private Connection conexion;
+
+    static{
+        try{
+            InputStream in = databaseConection.class.getResourceAsStream("/db.properties");
+            reader.load(in);
+
+        }catch(Exception e){
+            System.out.println("Error: "+e.getMessage());
+        }
+    }
+    private databaseConection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = reader.getProperty("db.url");
+            String username = reader.getProperty("db.user");
+            String password = reader.getProperty("db.password");
+            conexion = DriverManager.getConnection(url,username,password);
+        } catch (SQLException | ClassNotFoundException e) {
+            System.err.println("Error al conectar a la bd: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    public static databaseConection getInstancia() {
+        try {
+            if (instancia == null || instancia.conexion.isClosed()) {
+                instancia = new databaseConection();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return instancia;
+    }
+
+    public Connection getConnection() {
+        return conexion;
+    }
+}
---
+++ src/main/webapp/Evento/evento.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: maxim
+  Date: 29/10/2025
+  Time: 19:47
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<header class="login__encabezado alinear-centro-column gap-2">
+    <h1 class="login__marca titillium-negra texto-dorado m-0">GOLDEN GYM</h1>
+    <img class="login__logo" src="${pageContext.request.contextPath}/assets/img/icon2.svg" alt="Golden Gym">
+</header>
+
+</body>
+</html>
